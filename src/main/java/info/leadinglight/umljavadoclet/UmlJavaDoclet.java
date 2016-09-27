24b72805387c2e61e853e5ca3f51a0900920b234
Gerald Boersma <gerald.boersma@gmail.com>
2016-09-27 10:49:33 -0700

    Refactor the doclet to generate Javadoc and add placeholders for TODOs.

--- src/main/java/info/leadinglight/umljavadoclet/UmlJavaDoclet.java
+++ src/main/java/info/leadinglight/umljavadoclet/UmlJavaDoclet.java
@@ -8,32 +8,31 @@ import info.leadinglight.umljavadoclet.printer.PackageDiagramPrinter;
 import info.leadinglight.umljavadoclet.model.Model;
 import info.leadinglight.umljavadoclet.model.ModelClass;
 import info.leadinglight.umljavadoclet.model.ModelPackage;
-import info.leadinglight.umljavadoclet.printer.ModelPrinter;
 import info.leadinglight.umljavadoclet.printer.OverviewDiagramPrinter;
 
 public class UmlJavaDoclet {
     public static boolean start(RootDoc root) {
+        // Generate Javadocs using standard doclet.
+        System.out.println("Generating Javadocs...");
+        generateJavadoc(root);
+        
+        // Extract the Model.
         Model model = new Model(root);
         model.map();
         
-        // Dump results to file.
-        ModelPrinter modelPrinter = new ModelPrinter(model);
-        modelPrinter.generate();
-        modelPrinter.toFile("/Users/gerald/tmp/umljavadoclet/model.out");
+        // Generate the diagrams.
+        System.out.println("Generating diagrams...");
+        generateContextDiagrams(root, model);
+        generatePackageDiagrams(root, model);
+        generateOverviewDiagram(root, model);
         
-        // Generate the PUML for the context diagrams.
-        for (ModelClass modelClass: model.classes()) {
-            if (modelClass.isInternal()) {
-                generateContextDiagram(model, modelClass);
-            }
-        }
+        // Execute PlantUML.
+        executePlantUML(root, model);
         
-        // Generate the PUML for the package diagrams.
-        for (ModelPackage modelPackage: model.packages()) {
-            generatePackageDiagram(model, modelPackage);
-        }
+        // Update the Javadocs with the generated diagrams / maps.
+        System.out.println("Updating Javadocs...");
+        updateJavadocs(root, model);
         
-        generateOverviewDiagram(model);
         return true;
     }
 
@@ -47,24 +46,50 @@ public class UmlJavaDoclet {
         return Standard.languageVersion();
     }
     
-    private static void generateContextDiagram(Model model, ModelClass modelClass) {
+    private static void generateJavadoc(RootDoc rootDoc) {
+        Standard.start(rootDoc);
+    }
+    
+    private static void generateContextDiagrams(RootDoc rootDoc, Model model) {
+        for (ModelClass modelClass: model.classes()) {
+            if (modelClass.isInternal()) {
+                generateContextDiagram(rootDoc, model, modelClass);
+            }
+        }
+    }
+    
+    private static void generateContextDiagram(RootDoc rootDoc, Model model, ModelClass modelClass) {
         ContextDiagramPrinter generator = new ContextDiagramPrinter(model, modelClass);
         String filename = "/Users/gerald/tmp/umljavadoclet/puml/context_" + modelClass.fullNameWithoutParameters().replace(".", "_") + ".puml";
         generator.generate();
         generator.toFile(filename);
     }
+    
+    private static void generatePackageDiagrams(RootDoc rootDoc, Model model) {
+        for (ModelPackage modelPackage: model.packages()) {
+            generatePackageDiagram(rootDoc, model, modelPackage);
+        }
+    }
 
-    private static void generatePackageDiagram(Model model, ModelPackage modelPackage) {
+    private static void generatePackageDiagram(RootDoc rootDoc, Model model, ModelPackage modelPackage) {
         PackageDiagramPrinter generator = new PackageDiagramPrinter(model, modelPackage);
         String filename = "/Users/gerald/tmp/umljavadoclet/puml/package_" + modelPackage.fullName().replace(".", "_") + ".puml";
         generator.generate();
         generator.toFile(filename);
     }
 
-    private static void generateOverviewDiagram(Model model) {
+    private static void generateOverviewDiagram(RootDoc rootDoc, Model model) {
         OverviewDiagramPrinter generator = new OverviewDiagramPrinter(model);
         String filename = "/Users/gerald/tmp/umljavadoclet/puml/overview.puml";
         generator.generate();
         generator.toFile(filename);
+    }
+    
+    private static void executePlantUML(RootDoc rootDoc, Model model) {
+        // TODO Call PlantUML to generate the diagrams.
+    }
+    
+    private static void updateJavadocs(RootDoc rootDoc, Model model) {
+        // TODO Update all of the HTML to add the generated diagrams.
     }
 }
