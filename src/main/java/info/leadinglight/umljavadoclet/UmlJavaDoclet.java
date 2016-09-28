ce0a850bc9a4b7beb6b849f95e8299627d3d181b
Gerald Boersma <gerald.boersma@gmail.com>
2016-09-27 17:27:55 -0700

    Embed the diagrams into the Javadoc pages.

--- src/main/java/info/leadinglight/umljavadoclet/UmlJavaDoclet.java
+++ src/main/java/info/leadinglight/umljavadoclet/UmlJavaDoclet.java
@@ -10,10 +10,16 @@ import info.leadinglight.umljavadoclet.model.ModelClass;
 import info.leadinglight.umljavadoclet.model.ModelPackage;
 import info.leadinglight.umljavadoclet.printer.OverviewDiagramPrinter;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.regex.Pattern;
 import net.sourceforge.plantuml.FileFormat;
 import net.sourceforge.plantuml.FileFormatOption;
 import net.sourceforge.plantuml.SourceStringReader;
@@ -71,7 +77,14 @@ public class UmlJavaDoclet {
         File file = createFile(modelClass.packageName(), modelClass.shortNameWithoutParameters(), "puml");
         boolean success = generator.toFile(file);
         if (success && executePlantUML(modelClass.packageName(), modelClass.shortNameWithoutParameters(), generator.stringBuilder())) {
-            System.out.println("Generated diagram for class " + modelClass.fullName());
+            if (updateHtml(
+                    fileForName(modelClass.packageName()), 
+                    modelClass.shortNameWithoutParameters(), 
+                    Pattern.compile(".*(Class|Interface|Enum) " + modelClass.shortNameWithoutParameters() + ".*"))) {
+                System.out.println("Generated diagram for class " + modelClass.fullName());
+            } else {
+                System.out.println("ERROR: Could not update html page for class " + modelClass.fullName());
+            }
         } else {
             System.out.println("ERROR: Could not generate diagram for class " + modelClass.fullName());
         }
@@ -86,10 +99,17 @@ public class UmlJavaDoclet {
     private static void generatePackageDiagram(RootDoc rootDoc, Model model, ModelPackage modelPackage) {
         PackageDiagramPrinter generator = new PackageDiagramPrinter(model, modelPackage);
         generator.generate();
-        File file = createFile(modelPackage.fullName(), "package", "puml");
+        File file = createFile(modelPackage.fullName(), "package-summary", "puml");
         boolean success = generator.toFile(file);
-        if (success && executePlantUML(modelPackage.fullName(), "package", generator.stringBuilder())) {
-            System.out.println("Generated diagram for package " + modelPackage.fullName());
+        if (success && executePlantUML(modelPackage.fullName(), "package-summary", generator.stringBuilder())) {
+            if (updateHtml(
+                    fileForName(modelPackage.fullName()), 
+                    "package-summary",
+                    Pattern.compile("(</[Hh]2>)|(<h1 title=\"Package\").*"))) {
+                System.out.println("Generated diagram for package " + modelPackage.fullName());
+            } else {
+                System.out.println("ERROR: Could not update html page for package " + modelPackage.fullName());
+            }
         } else {
             System.out.println("ERROR: Could not generate diagram for package " + modelPackage.fullName());
         }
@@ -98,10 +118,17 @@ public class UmlJavaDoclet {
     private static void generateOverviewDiagram(RootDoc rootDoc, Model model) {
         OverviewDiagramPrinter generator = new OverviewDiagramPrinter(model);
         generator.generate();
-        File file = createFile("", "overview", "puml");
+        File file = createFile("", "overview-summary", "puml");
         boolean success = generator.toFile(file);
-        if (success && executePlantUML("", "overview", generator.stringBuilder())) {
-            System.out.println("Generated overview diagram");
+        if (success && executePlantUML("", "overview-summary", generator.stringBuilder())) {
+            if (updateHtml(
+                    fileForName(""),
+                    "overview-summary",
+                    Pattern.compile("<div class=\"contentContainer\">"))) {
+                System.out.println("Generated overview diagram");
+            } else {
+                System.out.println("ERROR: Could not update html page for overview diagram");
+            }
         } else {
             System.out.println("ERROR: Could not generate overview diagram");
         }
@@ -149,4 +176,72 @@ public class UmlJavaDoclet {
         }
         return file;
     }
+    
+    private static boolean updateHtml(File directory, String baseName, Pattern insertPointPattern) {
+        File htmlFile = new File(directory, baseName + ".html");
+        if (!htmlFile.exists()) {
+            System.out.println("ERROR: Could not find html file " + htmlFile.getName());
+            return false;
+        }
+
+        File svgFile = new File(directory, baseName + ".svg");
+        if (!svgFile.exists()) {
+            System.out.println("ERROR: Could not find svg file " + svgFile.getName());
+            return false;
+        }
+
+        File updatedHtml = new File(directory, baseName + ".uml");
+
+        boolean matched = false;
+        BufferedWriter writer = null;
+        BufferedReader reader = null;
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(updatedHtml), "UTF-8"));
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(htmlFile), "UTF-8"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.newLine();
+                if (!matched && insertPointPattern.matcher(line).matches()) {
+                    matched = true;
+                    String tag = String.format(UML_DIV_TAG, baseName);
+                    writer.newLine();
+                    writer.write(tag);
+                    writer.newLine();
+                }
+            }
+        } catch (IOException e) {
+            //e.printStackTrace();
+            return false;
+        } finally {
+            try {
+                if (writer != null)
+                    writer.close();
+                if (reader != null)
+                    reader.close();
+            } catch(IOException e) {
+                //e.printStackTrace();
+                return false;
+            }
+        }
+        
+	// if altered, delete old file and rename new one to the old file name
+	if (matched) {
+	    htmlFile.delete();
+	    updatedHtml.renameTo(htmlFile);
+            return true;
+	} else {
+            System.out.println("ERROR: Could not insert diagram into HTML file " + htmlFile.getName());
+	    htmlFile.delete();
+            return false;
+	}
+    }
+
+    // TODO Not specifying the width and height of diagrams means that they may be bigger than the page.
+    // However, if I specify a width and height, the diagrams do not resize if I zoom in and out.
+    // Need to investigate how to do this to keep the diagrams consistent and the browser happy.
+    private static final String UML_DIV_TAG = 
+	"<div align=\"center\">" +
+	    "<object type=\"image/svg+xml\" data=\"%1$s.svg\" alt=\"Package class diagram package %1$s\" border=0></object>" +
+	"</div>";
 }
